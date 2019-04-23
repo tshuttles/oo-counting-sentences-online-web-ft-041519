8133a0d6bf41e231c2ad0d359cf9b8b350bafa24
Tachi Shuttlesworth <tshuttlesworth@gmail.com>
2019-04-23 22:23:38 +0000

    Automatically backed up by Learn

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,31 @@ require 'pry'
 class String
 
   def sentence?
-    
+    if self.end_with?(".")
+      return true 
+    else 
+      return false 
+    end 
   end
 
   def question?
-
+    if self.end_with?("?")
+      return true 
+    else 
+      return false
+    end 
   end
 
   def exclamation?
-
+    if self.end_with?("!")
+      return true 
+    else 
+      return false
+    end 
   end
 
   def count_sentences
-
+    binding.pry 
+    self.split().count
   end
 end
